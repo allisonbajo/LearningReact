fd79b88a791345075b09aa03835be8631cc00a83
Allison Bajo <allisonbajo@gmail.com>
2025-09-28 23:41:45 -0700

    Separated the different components to their own file

--- Section03/01-starting-project/src/App.jsx
+++ Section03/01-starting-project/src/App.jsx
@@ -1,41 +1,12 @@
-import reactImg from './assets/react-core-concepts.png';
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
+
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 
 function App() {
+  // This is the syntax for the splitting of data elements into the fields.
+  // <CoreConcept {... CORE_CONCEPTS[1]}
   return (
     <div>
       <Header />
---
+++ Section03/01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,24 @@
+// This is the deconstructed way of using props in the component
+//function CoreConcept({title, description, image}) {
+//   return (
+//     <li>
+//       <img src={image} alt={title} />
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </li>
+//   );
+// }
+
+// This is the default props use
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+
+export default CoreConcept;
