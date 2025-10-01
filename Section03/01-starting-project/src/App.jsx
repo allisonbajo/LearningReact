2d0c4689188bc250fd1a164dd3017eca753b14e0
Allison Bajo <allisonbajo@gmail.com>
2025-10-01 16:34:54 -0700

    Added conditional JSX to control what is displayed

--- Section03/01-starting-project/src/App.jsx
+++ Section03/01-starting-project/src/App.jsx
@@ -5,12 +5,13 @@ import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from './components/TabButton.jsx'
 
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 
 
 function App() {
   console.log('[App] Rendered')
 
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState();
   //let tabContent = 'Please click a button';
 
   function handleSelect(selectedButton) {
@@ -50,7 +51,18 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+            {!selectedTopic ? <p>Please select a topic</p> : null} 
+            {selectedTopic ? (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+            ) : null}
         </section>
       </main>
     </div>
