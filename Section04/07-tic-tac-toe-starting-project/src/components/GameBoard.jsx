f58395195f5bfa87780df9bc4eaf27030764a800
Allison Bajo <allisonbajo@gmail.com>
2025-10-06 14:11:33 -0700

    Building the tic-tac-toe app

---
+++ Section04/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+function GameBoard() {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        console.log(`row ${rowIndex}, col ${colIndex}`)
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex, colIndex] = "X";
+            return updatedGameBoard;
+        });
+    }
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
+}
+
+
+export default GameBoard;
---
+++ Section04/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+    const [ playerName, setPlayerName ] = useState(initialName);
+    const [ isEditing, setIsEditing] = useState(false);
+
+    // WARNING: Special consideration needed when updating state based on a previous value
+    // setIsEditing(!isEditing); is not recommended
+    // Use an anonymous function instead
+    // Calling the function will force React to get the actual current value
+    // setIsEditing((editing) => !editing);
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        console.log(event.target.value);
+        setPlayerName(event.target.value);
+    }
+
+    // Original code for player-name conditional
+    //{ isEditing
+        //? <input />
+        //: <span className="player-name">{name}</span>
+    //}
+
+    // TIP: Placing the conditional JS code out of the markup will result
+    // in cleaner code
+    // let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // if (isEditing) {
+    //     editablePlayerName = <input type="text" required value={playerName} />
+    // }
+    // Another way to express this
+
+    // value will always set the value and prevents the user from changing it
+    // defaultValue will provide an initial value and the user can change it
+    // NOTE: There is a better way using React
+    let editablePlayerName = isEditing
+        ? <input type="text" value={playerName} onChange={handleChange} required />
+        : <span className="player-name">{playerName}</span>;
+
+
+    let buttonText = isEditing ? "Save" : "Edit";
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>
+                {buttonText}
+            </button>
+        </li>
+    );
+}
+
+
+export default Player;
