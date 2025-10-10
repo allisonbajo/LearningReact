3c055a3919f0f76dc3404f93ca2ac7e3d2651c6b
Allison Bajo <allisonbajo@gmail.com>
2025-10-09 23:38:54 -0700

    Practice Project: Investment Calculator

---
+++ Section05/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (<>
+        <header id="header">
+            <img src="investment-calculator-logo.png" alt="A simple logo" />
+            <h1>Investment Calculator</h1>
+        </header>
+    </>);
+}
+
+
+export default Header;
---
+++ Section05/01-starting-project/src/components/Result.jsx
@@ -0,0 +1,23 @@
+function Result() {
+    return (
+        <div id="result">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Investment Value</th>
+                        <th>Interest (Year)</th>
+                        <th>TOtal Interest</th>
+                        <th>Invested Capital</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+
+export default Result;
