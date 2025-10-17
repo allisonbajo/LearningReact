c6ed20e99ed5b475b83e3186a4e5fc0c7ba3a343
Allison Bajo <allisonbajo@gmail.com>
2025-10-17 16:14:49 -0700

    Dynamic styles

--- Section06/01-starting-project/src/components/Header.jsx
+++ Section06/01-starting-project/src/components/Header.jsx
@@ -2,19 +2,19 @@ import logo from '../assets/logo.png';
 import classes from './Header.module.css';
 
 export default function Header() {
-  // Setting inline styles in JSX is similar to HTML
-  // Add attribute "style" with a dynamic object
-  // NOTE: dashes are not supported as attribute names. Use camel casing
-  // style={ {<object>} }
-  // style = {{
-  //     color: "red",
-  //     textAlign: ...,
-  // }}
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
-    </header>
-  );
+    // Setting inline styles in JSX is similar to HTML
+    // Add attribute "style" with a dynamic object
+    // NOTE: dashes are not supported as attribute names. Use camel casing
+    // style={ {<object>} }
+    // style = {{
+    //     color: "red",
+    //     textAlign: ...,
+    // }}
+    return (
+        <header>
+            <img src={logo} alt="A canvas" />
+            <h1>ReactArt</h1>
+            <p className={classes.paragraph}>A community of artists and art-lovers.</p>
+        </header>
+    );
 }
