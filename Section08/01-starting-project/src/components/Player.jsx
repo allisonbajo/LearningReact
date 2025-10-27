9a37b097dea03ddf226b9909de89e45a86cedfc2
Allison Bajo <allisonbajo@gmail.com>
2025-10-27 14:28:15 -0700

    First use of Ref. Pretty lean.

--- Section08/01-starting-project/src/components/Player.jsx
+++ Section08/01-starting-project/src/components/Player.jsx
@@ -1,27 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 export default function Player() {
-  const [ enteredName, setEnteredName ] = useState('');
+  const [ enteredName, setEnteredName ] = useState(null);
   // const [ playerName, setPlayerName ] = useState('unknown entity');
-  const [ submitted, setSubmitted ] = useState(false);
+  // const [ submitted, setSubmitted ] = useState(false);
 
-  function handleChange(event) {
-    setEnteredName(event.target.value);
-    console.log(`Name entered: ${event.target.value}`);
-  }
+  const playerName = useRef();
+
+  // function handleChange(event) {
+  //   console.log(`Name entered: ${event.target.value}`);
+  //   setSubmitted(false);
+  //   setEnteredName(event.target.value);
+  // }
 
   function handleSetName() {
     console.log('Set Name clicked');
     // setPlayerName(enteredName);
-    setSubmitted(true);
+    // setSubmitted(true);
+    setEnteredName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2>
+      {/* <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2> */}
+      <h2>Welcome {enteredName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
