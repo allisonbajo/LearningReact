17df91bfc00eca9e10b8193c356032b4bd55331c
Allison Bajo <allisonbajo@gmail.com>
2025-10-27 14:17:49 -0700

    Updated Player component to set name

--- Section08/01-starting-project/src/components/Player.jsx
+++ Section08/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
+
 export default function Player() {
+  const [ enteredName, setEnteredName ] = useState('');
+  // const [ playerName, setPlayerName ] = useState('unknown entity');
+  const [ submitted, setSubmitted ] = useState(false);
+
+  function handleChange(event) {
+    setEnteredName(event.target.value);
+    console.log(`Name entered: ${event.target.value}`);
+  }
+
+  function handleSetName() {
+    console.log('Set Name clicked');
+    // setPlayerName(enteredName);
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredName} />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
